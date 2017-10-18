5be003388d3d009afeb8c7dce325e91660b2647e
pengxp <pengxp@by-health.com>
2017-10-18 14:58:29 +0800

    React 16 was moving PropTypes into prop-types packages

--- src/Picker.js
+++ src/Picker.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import PickerColumn from './PickerColumn';
 
 class Picker extends React.Component {
